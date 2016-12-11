cb35825307907816db92c43db802c682be91860a
JoeProgram <joseph.france@gmail.com>
2016-12-11 14:17:52 -0800

    Another level, title screen

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour {
+
+	public Image blackout;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(StartHelper());
+	}
+
+	IEnumerator StartHelper(){
+
+		yield return new WaitForSeconds(0.25f);
+
+		DOTween.To(
+			() => blackout.color,
+			x => blackout.color = x,
+			new Color(0,0,0,0),
+			1f);
+		
+		yield return new WaitForSeconds(3);
+
+		DOTween.To(
+			() => blackout.color,
+			x => blackout.color = x,
+			Color.black,
+			1f);
+
+		yield return new WaitForSeconds(1.25f);
+
+		//load next level
+		SceneManager.LoadScene(  (SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
+
+
+	}
+
+}
